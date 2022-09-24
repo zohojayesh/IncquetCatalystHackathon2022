d4f87650200d341c6eb8469c30cf52d3ef7a300a
rashmi05 <rashmi959525@gmail.com>
2022-09-24 20:09:58 +0530

    navbar logo

--- client/home/components/Nav.vue
+++ client/home/components/Nav.vue
@@ -8,9 +8,9 @@
   >
     <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
     <v-toolbar-title style="width:350px">
-      <img class="mt-1" src="https://res.cloudinary.com/incquet-solution/image/upload/v1664027690/Incquet%20website/New%20Website%20Design%20/LogoWhite-Incquet_ibtaxx.png"
-       width="18%">
-      <span>Incquet Solutions</span>
+      <img class="mt-1" src="https://res.cloudinary.com/incquet-solution/image/upload/v1580409178/Incquet%20website/zoho_logo_white_kinwcg.png"
+       width="75%">
+      
       </v-toolbar-title>
     <v-tabs right>
       <v-tab to="/" > <v-icon>mdi-home</v-icon> &nbsp; Home</v-tab>
